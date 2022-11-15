c5abd1740e614a44a95bfb9665e44566d981664e
nuelobeto <nuelobeto@gmail.com>
2022-11-15 17:09:16 +0100

    home page ui fixes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,19 @@ import Main from "./../components/Main";
 import "../assets/styles/Home.scss";
 
 const Home = () => {
+  const stories = [1, 2, 3, 4, 5, 6, 7, 8];
+
   return (
     <Main>
       <div className="home_wrapper">
         <div className="home_main">
-          <div className="stories"></div>
+          <div className="stories">
+            <div>
+              {stories.map((item, index) => (
+                <div key={index} className="story"></div>
+              ))}
+            </div>
+          </div>
 
           <div className="posts">
             <div className="post"></div>
