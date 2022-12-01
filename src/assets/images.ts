7df38d37a018e4df90cd86e1c23d491a5b10a245
nuelobeto <nuelobeto@gmail.com>
2022-12-01 15:18:32 +0100

    integrated cloudinary

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -4,62 +4,119 @@ export const USER1 =
   "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900313/friendzone/user1_zzxg28.jpg";
 export const USER2 =
   "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900324/friendzone/user2_d63wod.jpg";
-export const USER3 = "/images/user3.jpg";
-export const USER4 = "/images/user4.jpg";
-export const USER5 = "/images/user5.jpg";
-export const USER6 = "/images/user6.jpg";
-export const USER7 = "/images/user7.jpg";
-export const USER8 = "/images/user8.jpg";
-export const USER9 = "/images/user9.jpg";
-export const USER10 = "/images/user10.jpg";
+export const USER3 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900303/friendzone/user3_uhfahn.jpg";
+export const USER4 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900299/friendzone/user4_isrvlv.jpg";
+export const USER5 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900296/friendzone/user5_dknyq7.jpg";
+export const USER6 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900308/friendzone/user6_oemfm3.jpg";
+export const USER7 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900300/friendzone/user7_vxij0f.jpg";
+export const USER8 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900313/friendzone/user8_gavee0.jpg";
+export const USER9 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900310/friendzone/user9_tht1p1.jpg";
+export const USER10 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900267/friendzone/user10_gghysf.jpg";
 export const POST1 =
   "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900262/friendzone/post1_cmj2b2.jpg";
-export const POST2 = "/images/post2.png";
-export const POST3 = "/images/post3.jpg";
-export const POST4 = "/images/post4.jpg";
-export const POST5 = "/images/post5.png";
-export const POST6 = "/images/post6.jpg";
-export const POST7 = "/images/post7.jpg";
-export const POST8 = "/images/post8.jpg";
-export const POST9 = "/images/post9.jpg";
-export const POST10 = "/images/post10.jpg";
-export const POST11 = "/images/post11.jpg";
-export const POST12 = "/images/post12.jpg";
-export const POST13 = "/images/post13.png";
-export const POST14 = "/images/post14.jpg";
-export const POST15 = "/images/post15.jpg";
-export const POST16 = "/images/post16.jpg";
-export const POST17 = "/images/post17.jpg";
-export const POST18 = "/images/post18.jpg";
-export const POST19 = "/images/post19.jpg";
-export const POST20 = "/images/post20.jpg";
-export const POST21 = "/images/post21.jpg";
-export const POST22 = "/images/post22.jpg";
-export const POST23 = "/images/post23.jpg";
-export const POST24 = "/images/post24.jpg";
-export const POST25 = "/images/post25.jpg";
-export const POST26 = "/images/post26.jpg";
-export const POST27 = "/images/post27.jpg";
-export const POST28 = "/images/post28.jpg";
-export const POST29 = "/images/post29.jpg";
-export const POST30 = "/images/post30.jpg";
-export const POST31 = "/images/post31.jpg";
-export const POST32 = "/images/post32.jpg";
-export const POST33 = "/images/post33.jpg";
-export const POST34 = "/images/post34.png";
-export const POST35 = "/images/post35.jpg";
-export const POST36 = "/images/post36.jpg";
-export const POST37 = "/images/post37.jpg";
-export const POST38 = "/images/post38.png";
-export const POST39 = "/images/post39.jpg";
-export const POST40 = "/images/post40.jpg";
-export const POST41 = "/images/post41.jpg";
-export const POST42 = "/images/post42.jpg";
-export const POST43 = "/images/post43.jpg";
-export const POST44 = "/images/post44.jpg";
-export const POST45 = "/images/post45.png";
-export const POST46 = "/images/post46.png";
-export const POST47 = "/images/post47.jpg";
-export const POST48 = "/images/post48.png";
-export const POST49 = "/images/post49.jpg";
-export const POST50 = "/images/post50.jpg";
+export const POST2 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900263/friendzone/post2_anpcck.png";
+export const POST3 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900261/friendzone/post3_y7s5x5.jpg";
+export const POST4 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900262/friendzone/post4_o43o8u.jpg";
+export const POST5 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900265/friendzone/post5_otfdqv.png";
+export const POST6 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900264/friendzone/post6_pjiysg.jpg";
+export const POST7 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900264/friendzone/post7_x195am.jpg";
+export const POST8 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900265/friendzone/post8_abrrzc.jpg";
+export const POST9 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900267/friendzone/post9_xv4rss.jpg";
+export const POST10 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900268/friendzone/post10_dj4o4k.jpg";
+export const POST11 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900271/friendzone/post11_dvpnsz.jpg";
+export const POST12 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900266/friendzone/post12_v0pkah.jpg";
+export const POST13 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900269/friendzone/post13_wtae8l.png";
+export const POST14 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900270/friendzone/post14_yb5hui.jpg";
+export const POST15 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900269/friendzone/post15_y0zbpo.jpg";
+export const POST16 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900270/friendzone/post16_vdttpx.jpg";
+export const POST17 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900271/friendzone/post17_ricsxv.jpg";
+export const POST18 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900273/friendzone/post18_qrmuup.jpg";
+export const POST19 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900271/friendzone/post19_qimcwv.jpg";
+export const POST20 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900270/friendzone/post20_tf3bx4.jpg";
+export const POST21 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900277/friendzone/post21_bevlf0.jpg";
+export const POST22 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900272/friendzone/post22_tkze7r.jpg";
+export const POST23 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900272/friendzone/post23_jq3xq6.jpg";
+export const POST24 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900273/friendzone/post24_wyxj9v.jpg";
+export const POST25 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900274/friendzone/post25_nq8ubw.jpg";
+export const POST26 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900276/friendzone/post26_lre1qx.jpg";
+export const POST27 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900274/friendzone/post27_hhllny.jpg";
+export const POST28 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900275/friendzone/post28_cqag7a.jpg";
+export const POST29 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900276/friendzone/post29_snld9o.jpg";
+export const POST30 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900309/friendzone/post30_qjbxbk.jpg";
+export const POST31 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900276/friendzone/post31_ta7e4b.jpg";
+export const POST32 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900277/friendzone/post32_zl5yqk.jpg";
+export const POST33 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900276/friendzone/post33_f6ivya.jpg";
+export const POST34 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900287/friendzone/post34_szpgt4.png";
+export const POST35 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900278/friendzone/post35_i0zlrk.jpg";
+export const POST36 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900278/friendzone/post36_idmlwe.jpg";
+export const POST37 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900282/friendzone/post37_sm7jsp.jpg";
+export const POST38 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900286/friendzone/post38_xeeqbd.png";
+export const POST39 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900277/friendzone/post39_esqbef.jpg";
+export const POST40 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900280/friendzone/post40_c5vou7.jpg";
+export const POST41 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900280/friendzone/post41_xozqss.jpg";
+export const POST42 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900282/friendzone/post42_nyxzwd.jpg";
+export const POST43 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900282/friendzone/post43_akmeje.jpg";
+export const POST44 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900281/friendzone/post44_qjfmlq.jpg";
+export const POST45 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900285/friendzone/post45_n6ocef.png";
+export const POST46 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900292/friendzone/post46_o96xgn.png";
+export const POST47 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900285/friendzone/post47_zac2ol.jpg";
+export const POST48 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900294/friendzone/post48_seeul4.png";
+export const POST49 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900288/friendzone/post49_ny8guo.jpg";
+export const POST50 =
+  "https://res.cloudinary.com/dk9bt9lkn/image/upload/v1669900288/friendzone/post50_m4aiqz.jpg";
